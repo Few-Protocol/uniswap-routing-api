ff5fef39bcf95ef6c11895cfeb5e3d379779fd85
DreamXzxy <17998878+DreamXzxy@users.noreply.github.com>
2026-09-21 17:44:16 +0800

    fix: route quotes with the selected Universal Router version

--- lib/handlers/router-entities/route-caching/dynamo-route-caching-provider.ts
+++ lib/handlers/router-entities/route-caching/dynamo-route-caching-provider.ts
@@ -22,7 +22,7 @@ import { PromiseResult } from 'aws-sdk/lib/request'
 import { DEFAULT_BLOCKS_TO_LIVE_ROUTES_DB } from '../../../util/defaultBlocksToLiveRoutesDB'
 import { getSymbolOrAddress } from '../../../util/getSymbolOrAddress'
 import { serializeRouteIds } from '@ring-protocol/smart-order-router/build/main/util/serializeRouteIds'
-import { UniversalRouterVersion } from '@ring-protocol/universal-router-sdk'
+import { UNIVERSAL_ROUTER_VERSION, UniversalRouterVersion } from '@ring-protocol/universal-router-sdk'
 import { computeProtocolsInvolvedIfMixed } from '../../../util/computeProtocolsInvolvedIfMixed'
 
 interface ConstructorParams {
@@ -159,6 +159,7 @@ export class DynamoRouteCachingProvider extends IRouteCachingProvider {
               // only when UR header is v1.2, we explicitly delete the V4 from protocols in
               // https://github.com/Uniswap/smart-order-router/blob/main/src/routers/alpha-router/alpha-router.ts#L1461
               alphaRouterConfig?.universalRouterVersion === UniversalRouterVersion.V2_0 ||
+              alphaRouterConfig?.universalRouterVersion === UniversalRouterVersion.V2_1_1 ||
               // We want to roll out the mixed route with UR v1_2 with percent control,
               // along with the cached routes so that we can test the performance of the mixed route with UR v1_2ss
               (alphaRouterConfig?.enableMixedRouteWithUR1_2 &&
@@ -436,7 +437,7 @@ export class DynamoRouteCachingProvider extends IRouteCachingProvider {
     const payload = {
       headers: {
         'x-universal-router-version': protocols.includes(Protocol.V4)
-          ? UniversalRouterVersion.V2_0
+          ? (alphaRouterConfig?.universalRouterVersion ?? UNIVERSAL_ROUTER_VERSION(partitionKey.chainId))
           : UniversalRouterVersion.V1_2,
       },
       queryStringParameters: {
--- test/mocha/unit/util/supportedProtocolVersions.test.ts
+++ test/mocha/unit/util/supportedProtocolVersions.test.ts
@@ -1,4 +1,4 @@
-import { UniversalRouterVersion } from '@uniswap/universal-router-sdk'
+import { UniversalRouterVersion } from '@ring-protocol/universal-router-sdk'
 import {
   convertStringRouterVersionToEnum,
   protocolVersionsToBeExcludedFromMixed,
@@ -9,6 +9,7 @@ import { expect } from 'chai'
 describe('supported protocol versions', () => {
   it('should convert string router version to enum', async () => {
     expect(convertStringRouterVersionToEnum('1.2')).to.eq(UniversalRouterVersion.V1_2)
+    expect(convertStringRouterVersionToEnum('2.1.1')).to.eq(UniversalRouterVersion.V2_1_1)
     expect(convertStringRouterVersionToEnum('2.0')).to.eq(UniversalRouterVersion.V2_0)
     expect(convertStringRouterVersionToEnum('3.0')).to.eq(UniversalRouterVersion.V1_2)
     expect(convertStringRouterVersionToEnum(undefined)).to.eq(UniversalRouterVersion.V1_2)
@@ -16,6 +17,7 @@ describe('supported protocol versions', () => {
 
   it('should return protocol versions to be excluded from mixed', async () => {
     expect(protocolVersionsToBeExcludedFromMixed(UniversalRouterVersion.V1_2)).to.deep.eq([Protocol.V4])
+    expect(protocolVersionsToBeExcludedFromMixed(UniversalRouterVersion.V2_1_1)).to.deep.eq([])
     expect(protocolVersionsToBeExcludedFromMixed(UniversalRouterVersion.V2_0)).to.deep.eq([])
   })
 })
